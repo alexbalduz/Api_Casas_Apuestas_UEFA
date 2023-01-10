ec3a81585a010474e3096c2df325ac1ca4264a0b
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2023-01-10 17:53:12 +0100

    arreglos, nuevos csv

--- preparacion_datos/normalizacion.py
+++ preparacion_datos/normalizacion.py
@@ -11,59 +11,60 @@ listaEquipos = pd.read_csv('docs/equipo.csv')
 print(listaEquipos.head())
 
 #Vemos con qué tipo de datos trabajamos
-equipos=listaEquipos['Equipo']
+equipos = listaEquipos['Equipo']
 print(type(equipos))
 
 #Convertimos a lista
-equipos=list(equipos)
+equipos = list(equipos)
 print(type(equipos))
 print(equipos)
 
 #Normalizamos los nombres de los equipos
 for año in años:
-    data=pd.read_csv(f'datas/Champions/resultados{año}.csv')
+    df = pd.read_csv(f'datas/Champions/resultados_{año}.csv')
+    #La clave sera el equipo y el valor los goles
     diccionario={}
-    for nombre in data.Local.unique():
-        lista=str(nombre).lower().split()
-        if 'inter' in lista:
-            diccionario[nombre]='Internazionale'
+    for nombre in df.Local.unique():
+        list = str(nombre).lower().split()
+        if 'inter' in list:
+            diccionario[nombre] = 'Internazionale'
         for equipo in equipos:
-            if str(equipo).lower() in lista:
-                diccionario[nombre]=str(equipo)
-        if 'manchester' in lista:
-            if 'united' in lista:
-                diccionario[nombre]='Man. United'
-            elif 'city' in lista:
-                diccionario[nombre]='Man. City'
-        elif 'lille' in lista:
-            diccionario[nombre]='LOSC'
-        elif 'París' in lista:
-            diccionario[nombre]='Paris'
-        elif 'sporting' in lista:
-            if 'portugal' in lista:
-                diccionario[nombre]='Sporting CP'
-        elif 'donetsk' in lista:
-            diccionario[nombre]='Shakhtar Donetsk'
-        elif 'real' in lista:
-            if 'madrid' in lista:
-                diccionario[nombre]='Real Madrid'
-        elif 'young' in lista:
-            diccionario[nombre]='Young Boys'
+            if str(equipo).lower() in list:
+                diccionario[nombre] = str(equipo)
+        if 'manchester' in list:
+            if 'united' in list:
+                diccionario[nombre] = 'Man. United'
+            elif 'city' in list:
+                diccionario[nombre] = 'Man. City'
+        elif 'lille' in list:
+            diccionario[nombre] = 'LOSC'
+        elif 'París' in list:
+            diccionario[nombre] ='Paris'
+        elif 'sporting' in list:
+            if 'portugal' in list:
+                diccionario[nombre] = 'Sporting CP'
+        elif 'donetsk' in list:
+            diccionario[nombre] = 'Shakhtar Donetsk'
+        elif 'real' in list:
+            if 'madrid' in list:
+                diccionario[nombre] = 'Real Madrid'
+        elif 'young' in list:
+            diccionario[nombre] = 'Young Boys'
 
     for nombre in diccionario:
-        data["Local"] = data["Local"].replace(nombre,diccionario[nombre])
-        data["Visitante"] = data["Visitante"].replace(nombre,diccionario[nombre])
-    data.to_csv(f'Datos/resultados{año}.csv',index=False)
+        df["Local"] = df["Local"].replace(nombre,diccionario[nombre])
+        df["Visitante"] = df["Visitante"].replace(nombre,diccionario[nombre])
+    df.to_csv(f'datas/resultados{año}.csv',index=False)
 
 #Normalizamos los nombres de los equipos
-def NormalizacionNombres(palabra:str):
-    palabra=palabra.lower()
-    palabra=palabra.replace("á","a")
-    palabra=palabra.replace("é","e")
-    palabra=palabra.replace("í","i")
-    palabra=palabra.replace("ó","o")
-    palabra=palabra.replace("ö","o")
-    palabra=palabra.replace("ú","u")
-    palabra=palabra.replace("ü","u")
-    palabra=palabra.replace("ç","c")
-    return palabra
+def NormalizacionNombres(nombre:str):
+    nombre = nombre.lower()
+    nombre = nombre.replace("á","a")
+    nombre = nombre.replace("é","e")
+    nombre = nombre.replace("í","i")
+    nombre = nombre.replace("ó","o")
+    nombre = nombre.replace("ö","o")
+    nombre = nombre.replace("ú","u")
+    nombre = nombre.replace("ü","u")
+    nombre = nombre.replace("ç","c")
+    return nombre
--- preparacion_datos/pred.py
+++ preparacion_datos/pred.py
@@ -1,27 +1,27 @@
 import pandas as pd
 from normalizacion import NormalizacionNombres
 
-#Leemos el csv de equipos
-listaEquipos = pd.read_csv ('docs/equipo.csv')
-print(listaEquipos.head())
-
-#Vemos con qué tipo de datos trabajamos
-equipos=listaEquipos['Equipo']
-print(type(equipos))
-equipos=list(equipos)
-
 #Definimos todos los años como un array
 años=["2020-21","2019-20","2018-19","2017-18","2015-16","2014-15","2013-14","2012-13","2011-12","2010-11","2009-10","2008-09"
     ,"2007-08","2006-07","2005-06","2004-05","2003-04","2002-03","2001-02","2000-01"]
 Champions={}
 
+#Leemos el csv de equipos
+listaEquipos = pd.read_csv ('docs/equipo.csv')
+print(listaEquipos.head())
+
+#Vemos con qué tipo de datos trabajamos
+equipos = listaEquipos['Equipo']
+print(type(equipos))
+equipos=list(equipos)
+
 #Leemos los datos de la Champions
 for año in años:
-    Champions[f'{año}'] = pd.read_csv(f'Datos/Champions/resultados{año}.csv')
+    Champions[f'{año}'] = pd.read_csv(f'datas/Champions/resultados{año}.csv')
 
 #Diccionarios para guardar los goles de cada equipo
-golesLocal={}
-golesVisitante={}
+golesLocal = {}
+golesVisitante = {}
 
 #Inicializamos los diccionarios
 for equipo in equipos:
@@ -39,3 +39,4 @@ for año in años:
 
 print(golesLocal)
 print(golesVisitante)
+
